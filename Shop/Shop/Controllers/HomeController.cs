043f7b55d405cf68e69d968176000c4e55cac123
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-01-06 21:16:39 +0100

    #11
    - First Test of app accomplished
    - Creatign Database succeeded

--- Shop/Shop/Controllers/HomeController.cs
+++ Shop/Shop/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Shop.Contexts;
+using Shop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,15 @@ namespace Shop.Controllers
 {
     public class HomeController : Controller
     {
+        private BooksContext db = new BooksContext();
+        
         // GET: Home
         public ActionResult Index()
         {
-            
+            Category category = new Category { Name = "Criminal", Description = "xyz", IcoName = "criminal.png" };
+            db.Categories.Add(category);
+            db.SaveChanges();
+
             return View();
         }
     }
