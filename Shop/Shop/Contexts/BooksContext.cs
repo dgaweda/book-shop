2c329a0703e68b289ccb1ab77d62d73db9356675
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-01-06 20:36:22 +0100

    #10 Create Database connection
    - creates database connection
    - adds constructor when connection string is transfered
    -overrides saving tablenames (without "s")

--- Shop/Shop/Contexts/BooksContext.cs
+++ Shop/Shop/Contexts/BooksContext.cs
@@ -3,6 +3,7 @@ using Shop.Utility;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -10,12 +11,25 @@ namespace Shop.Contexts
 {
     public class BooksContext : DbContext
     {
+        
+        public BooksContext() : base("BooksContext") // Constructor when connectionstring(database) is extracted
+        {
+                
+            }
         // Added Tables
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderPosition> OrderPositions { get; set; }
         public DbSet<Address> Addresses { get; set; }
-        public DbSet<Client> Clients { get; set; } 
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>(); // Disables adding "s" convention to table names like Books + s = Bookss
+
+        }
     }
 }
