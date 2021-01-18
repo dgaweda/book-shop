38ed161cf0acb3cd015ee7f9efbcc62f42ea75f3
dgaweda <32791268+dgaweda@users.noreply.github.com>
2021-01-18 14:58:59 +0100

    Repairing
    rep

--- Shop/Shop/Migrations/Configuration.cs
+++ Shop/Shop/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-﻿namespace Shop.Migrations
+namespace Shop.Migrations
 {
     using Shop.Initializer;
     using System;
@@ -17,11 +17,6 @@
         protected override void Seed(Shop.Contexts.ShopContext context)
         {
             ShopInitilizer.SeedShopData(context);
-             
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
         }
     }
 }
